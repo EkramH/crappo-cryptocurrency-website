4b30f33b2813baeecb86482e5bb873c343048ce6
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-12 15:13:37 +0600

    aos in hero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
+
   return (
     <div>
       <Hero />
