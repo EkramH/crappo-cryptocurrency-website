5be9354bd5f5a0534a003164de03456465d353bc
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-13 12:26:28 +0600

    calc form done

--- src/components/calculate/Calculate.js
+++ src/components/calculate/Calculate.js
@@ -1,10 +1,29 @@
 import React from "react";
+import CalcForm from "./CalcForm";
 
 const Calculate = () => {
   return (
-    <div>
-      <h2>Calculate</h2>
-    </div>
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="text-center">
+          <h2
+            className="section-title"
+            data-aos="fade-up"
+            data-aos-offset="400"
+          >
+            Check hou much you can earn
+          </h2>
+          <p
+            className="section-subtitle text-lg mb-16 max-w-[622px] mx-auto"
+            data-aos="fade-up"
+            data-aos-delay="450"
+          >
+            Let's check your hash rate to see how much you will earn today.
+          </p>
+        </div>
+        <CalcForm />
+      </div>
+    </section>
   );
 };
 
