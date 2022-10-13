7d9e3e91c445215791f8095347e46eb063a5e8f1
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-13 16:31:49 +0600

    news latter form

--- src/components/newsLetter/NewsLetterForm.js
+++ src/components/newsLetter/NewsLetterForm.js
@@ -3,7 +3,16 @@ import React from "react";
 const NewsLetterForm = () => {
   return (
     <div>
-      <h3>News Letter Form</h3>
+      <form className="flex-1 flex flex-col items-center w-full gap-y-6 lg:flex-row lg:gap-x-10">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="input text-base text-white placeholder:text-white placeholder:text-base"
+        />
+        <button className="btn bg-white text-darkblue px-8 hover:bg-white/70">
+          Subscribe
+        </button>
+      </form>
     </div>
   );
 };
