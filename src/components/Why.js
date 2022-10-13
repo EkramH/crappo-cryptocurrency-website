dd1537f477f548623e052507e45665fd24640e2e
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-13 11:56:02 +0600

    why section done

--- src/components/Why.js
+++ src/components/Why.js
@@ -3,18 +3,27 @@ import whyImg from "../assets/img/why-img.png";
 
 const Why = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center gap-x-8 lg:flex-row">
+          <div
+            className="order-2 lg:order-1"
+            data-aos="fade-right"
+            data-aos-offset="400"
+          >
             <img src={whyImg} alt="" />
           </div>
-          <div>
-            <h2>Why should choose CRAPPO</h2>
-            <p>
+          <div
+            className="order-1 lg:order-2 max-w-[480px]"
+            data-aos="fade-left"
+            data-aos-offset="400"
+          >
+            <h2 className="section-title">Why should choose CRAPPO</h2>
+            <p className="section-subtitle">
               Experience the next generation cryptocurrency platform. No
               financial borders, extra fee, and fake review.
             </p>
+            <button className="btn px-6">Learn more</button>
           </div>
         </div>
       </div>
