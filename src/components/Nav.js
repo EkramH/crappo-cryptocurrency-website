7c21b449a3fb6d6f3b2247fc01abd23b9ba3053c
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-13 10:12:36 +0600

    nav desing done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { navData } from "../data";
 
 const Nav = () => {
   return (
-    <div>
-      <h2>Nav</h2>
-    </div>
+    <nav className="flex items-center">
+      <ul className="flex gap-x-8">
+        {navData.map((item, index) => {
+          return (
+            <li
+              key={index}
+              className="border-b-2 border-transparent hover:border-blue transition-all duration-300"
+            >
+              <a href={item.href}>{item.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
