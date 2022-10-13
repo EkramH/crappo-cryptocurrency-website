299b40a8259d93a0aa2fca231536af629b0ec1f4
Ekram Hosen <ekramhosen329@gmail.com>
2022-10-13 10:18:11 +0600

    login resiger

--- src/components/AccountBtns.js
+++ src/components/AccountBtns.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const AccountBtns = () => {
   return (
-    <div>
-      <h2>AccountBtns</h2>
+    <div className="flex items-center font-medium">
+      <a href="login" className="hover:text-blue transition">
+        Login
+      </a>
+      <span className="mx-6 text-white/20 font-thin">|</span>
+      <button className="btn h-[52px] text-base px-8">Register</button>
     </div>
   );
 };
